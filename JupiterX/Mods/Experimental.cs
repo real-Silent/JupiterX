fb8165da458f12e9d8291c705882e7890a062560
Novaafr <idk777800111@gmail.com>
2026-05-07 07:37:11 +0100

    remove unneeded code

--- JupiterX/Mods/Experimental.cs
+++ JupiterX/Mods/Experimental.cs
@@ -126,117 +126,6 @@ namespace JupiterX.Mods
             GorillaComputer.instance.OnConnectedToMasterStuff();
         }
 
-        public static void GetFuckedNetPlayers()
-        {
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -64.83f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -65.17f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.98f, -66.25f), Quaternion.Euler(270.50f, 180.00f, 180.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.17f, -66.25f), Quaternion.Euler(270.50f, 180.00f, 180.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.70f, -65.71f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.36f, -65.71f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.33f, -62.22f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -64.98f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -65.52f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.50f, -67.36f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -67.04f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.92f, -62.41f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.89f, -67.18f), Quaternion.Euler(46.59f, 180.00f, 180.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.75f, -67.31f), Quaternion.Euler(46.59f, 180.00f, 180.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.67f, -62.41f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.51f, -65.71f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.83f, -66.40f), Quaternion.Euler(323.86f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.34f, -66.38f), Quaternion.Euler(308.60f, 180.00f, 180.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.74f, -63.55f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.74f, -63.74f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.50f, -62.41f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -66.84f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.70f, -67.36f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.88f, -67.14f), Quaternion.Euler(48.74f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.17f, -67.36f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.36f, -67.36f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.90f, -66.96f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -65.33f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.19f, -66.26f), Quaternion.Euler(308.60f, 180.00f, 180.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.90f, -66.36f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.90f, -66.56f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.94f, -65.71f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.75f, -65.71f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.95f, -66.25f), Quaternion.Euler(323.86f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.84f, -62.41f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.50f, -61.26f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -64.13f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.00f, -62.60f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -63.54f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.52f, -65.33f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.52f, -64.98f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.52f, -65.17f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.52f, -64.63f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.52f, -65.52f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.52f, -64.83f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.00f, -62.41f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.16f, -62.41f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.50f, -62.60f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.49f, -63.55f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.76f, -63.74f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.67f, -62.60f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.84f, -60.95f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.49f, -63.74f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.76f, -63.55f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.84f, -62.60f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.84f, -61.31f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.23f, -63.74f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.00f, -62.24f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.33f, -61.26f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.23f, -63.55f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.74f, -62.41f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.95f, -63.55f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.95f, -63.74f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.00f, -62.05f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.16f, -62.60f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.84f, -60.70f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.00f, -61.75f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.00f, -61.94f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.67f, -61.26f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.33f, -62.00f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.26f, -61.52f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.87f, -61.26f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -63.16f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.33f, -62.60f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.04f, -65.71f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -65.71f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.98f, -60.48f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -63.74f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -65.52f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -65.33f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -63.93f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -64.98f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.33f, -62.41f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -65.17f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -63.35f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.92f, -62.60f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -64.63f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.74f, -62.60f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -64.83f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.98f, -64.63f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -66.44f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.85f, -66.63f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.17f, -65.71f), Quaternion.Euler(88.75f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.97f, -67.34f), Quaternion.Euler(48.74f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.82f, -67.22f), Quaternion.Euler(48.74f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.90f, -66.77f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.03f, -61.31f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.01f, -61.26f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.01f, -61.06f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.01f, -60.81f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.14f, -61.31f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 3.01f, -60.61f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.52f, -60.48f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.79f, -60.48f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 1.84f, -61.20f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-            PhotonNetwork.Instantiate("Network Player", new Vector3(-56.75f, 2.27f, -60.48f), Quaternion.Euler(0.00f, 0.00f, 0.00f));
-
-        }
-
         public static void GetFucked() 
         {
             PhotonNetwork.Instantiate("STICKABLE TARGET", new Vector3(-60.36f, 8.03f, -66.21f), Quaternion.Euler(40.00f, 90.00f, 0.00f));
@@ -384,48 +273,11 @@ namespace JupiterX.Mods
             PhotonNetwork.Instantiate("STICKABLE TARGET", new Vector3(-60.36f, 7.84f, -56.68f), Quaternion.Euler(40.00f, 90.00f, 0.00f));
             PhotonNetwork.Instantiate("STICKABLE TARGET", new Vector3(-60.36f, 8.00f, -57.05f), Quaternion.Euler(40.00f, 90.00f, 0.00f));
             PhotonNetwork.Instantiate("STICKABLE TARGET", new Vector3(-60.36f, 8.00f, -56.83f), Quaternion.Euler(40.00f, 90.00f, 0.00f));
+        }
+       
 
-        }
-        public static void eternalsugercookieSpammer()
-        {
-            if (Utility.RTrigger)
-            {
-                Utility.BetaSpawnPrefab("Eternal Sugar Cookie", Utility.RightHandTransform().position, Utility.RightHandTransform().rotation);
-            }
-        }
 
-        static bool spawnedMeowOnce = false;
-        static bool spawnedDickOnce = false;
-        public static void MeowMeowCubeSpawn()
-        {
-            if (Utility.RTrigger)
-            {
-                if (!spawnedMeowOnce)
-                {
-                    Utility.BetaSpawnPrefab("MeowMeowMeowMeow", Utility.RightHandTransform().position, Utility.RightHandTransform().rotation);
-                    spawnedMeowOnce = true;
-                }
-            }
-            else
-            {
-                spawnedMeowOnce = false;
-            }
-        }
-        public static void DickSpawn()
-        {
-            if (Utility.RTrigger)
-            {
-                if (!spawnedDickOnce)
-                {
-                    Utility.BetaSpawnPrefab("jstrumpchill", Utility.RightHandTransform().position, Utility.RightHandTransform().rotation);
-                    spawnedDickOnce = true;
-                }
-            }
-            else
-            {
-                spawnedDickOnce = false;
-            }
-        }
+
 
 
         // Console
