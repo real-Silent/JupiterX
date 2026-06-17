fb43136797e56cdea45e723f7df6be2a5d252b6d
Novaafr <idk777800111@gmail.com>
2026-06-17 15:22:23 +0100

    2.4.0

--- JupiterX/Utility.cs
+++ JupiterX/Utility.cs
@@ -1089,7 +1089,7 @@ namespace JupiterX
             }
         }
 
-        public static string version = "2.3.8"; 
+        public static string version = "2.4.0"; 
         public static string serverversion; 
         public static string minversion; 
         public static string discord; 
