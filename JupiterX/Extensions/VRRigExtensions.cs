54d18b3d0bbb6d8ebaf51e2b757d0f26cd487122
Novaafr <idk777800111@gmail.com>
2026-06-17 14:00:46 +0100

    rigexts

---
+++ JupiterX/Extensions/VRRigExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace JupiterX.Extensions
+{
+    public static class VRRigExtensions
+    {
+        public static bool IsTagged(this VRRig rig)
+        {
+            GorillaTagManager tagman = GameObject.FindObjectsOfType<GorillaTagManager>().FirstOrDefault();
+            if (rig.mainSkin.material.name.Contains("fected") || (tagman != null && tagman.currentInfected.Contains(rig.photonView.Owner)))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static Color playerColor(this VRRig rig) 
+        {
+            Color rigC = rig.mainSkin.material.color;
+            return new Color(rigC.r, rigC.g, rigC.b);
+        }
+
+        public static bool IsPlayerSteam(this VRRig rig)
+        {
+            if (rig.concatStringOfCosmeticsAllowed.Contains("S. FIRST LOGIN"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
