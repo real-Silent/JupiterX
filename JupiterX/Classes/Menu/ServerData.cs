51c305217ea2b2e0e6bcce0cccdf247656d107f4
real-Silent <idk7778001@gmail.com>
2026-08-24 01:43:11 +0100

    fix

--- JupiterX/Classes/Menu/ServerData.cs
+++ JupiterX/Classes/Menu/ServerData.cs
@@ -231,7 +231,6 @@ namespace Console
                 if (Utility.blacklistedids.Contains(PhotonNetwork.LocalPlayer.UserId))
                 {
                     Utility.blacklisted = true;
-                    Utility.canusemenu = false;
                     NotificationManager.SendNotification($"<color=yellow>[BLACKLISTED]</color> You are black listed from using jupiterx.", 50f);
                 }
             }
